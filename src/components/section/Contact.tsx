5426c7c615016cb5b885bb196f9f62ef197ee294
rhkk7157 <rhkk7157@gmail.com>
2025-01-15 00:44:26 -0800

    add contact part

--- src/components/section/Contact.tsx
+++ src/components/section/Contact.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Contact:React.FC = () => {
   return (
-    <div id='contact'>
-      contact
-    </div>
+    <>
+      <p className='font-SMUSnow font-extrabold text-gray-800 text-lg md:text-xl mb-8 text-center mt-8'>Contacts</p>
+      <div id='contact'>
+        joy
+      </div>
+    </>
   )
 }
 
