138073fd007d4fb2795eec5fc349b7dccfa41346
rhkk7157 <rhkk7157@gmail.com>
2025-01-14 17:22:20 -0800

    add Navbar font style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,15 @@ const Navbar: React.FC = () => {
     <nav className="w-full bg-white shadow-none">
       <div className="flex items-center justify-between px-6 py-4">
         <Link to="/" className="logo">
-          <img src="/logo.png" alt="Logo" className="max-w-[200px] h-auto"/>
+          <img src="/logo.png" alt="Logo" className="max-w-[200px] h-auto cursor-pointer"/>
         </Link>
         <ul className="flex space-x-12">
-          <li><Link to="home" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-medium no-underline">Home</Link></li>
-          <li><Link to="about" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-medium no-underline">About</Link></li>
-          <li><Link to="skills" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-medium no-underline">Skills</Link></li>
-          <li><Link to="projects" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-medium no-underline">Projects</Link></li>
-          <li><Link to="contact" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-medium no-underline">Contact</Link></li>
+          <li><Link to="home" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-black text-lg md:text-xl no-underline">Home</Link></li>
+          <li><Link to="about" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-black text-lg md:text-xl no-underline">About</Link></li>
+          <li><Link to="skills" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-black text-lg md:text-xl no-underline">Skills</Link></li>
+          <li><Link to="projects" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-black md:text-xl no-underline">Projects</Link></li>
+          <li><Link to="contact" smooth={true} duration={500} className="text-gray-800 hover:text-blue-500 font-black md:text-xl no-underline">Contact</Link></li>
+          {/* text-gray-800 hover:text-blue-500 font-medium no-underline */}
         </ul>
       </div>
     </nav>
