4a540db9c17bca3c0c7ad88407ae85cb7dc07c49
rhkk7157 <rhkk7157@gmail.com>
2025-01-15 00:44:20 -0800

    add projects part

--- src/components/section/Projects.tsx
+++ src/components/section/Projects.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const Projects:React.FC = () => {
   return (
-    <div id='projects'>
-      projects
-    </div>
+    <>
+      <p className='font-SMUSnow font-extrabold text-gray-800 text-lg md:text-xl mb-8 text-center mt-10'>Projects</p>
+      <div id="projects" className="grid grid-cols-3 gap-4 p-4">
+        <div className="project bg-gray-100 border border-gray-300 p-4 text-center">
+          Project 1
+        </div>
+        <div className="project bg-gray-100 border border-gray-300 p-4 text-center">
+          Project 2
+        </div>
+        <div className="project bg-gray-100 border border-gray-300 p-4 text-center">
+          Project 3
+        </div>
+        <div className="project bg-gray-100 border border-gray-300 p-4 text-center">
+          Project 4
+        </div>
+        <div className="project bg-gray-100 border border-gray-300 p-4 text-center">
+          Project 5
+        </div>
+        <div className="project bg-gray-100 border border-gray-300 p-4 text-center">
+          Project 6
+        </div>
+      </div>
+    </>
   )
 }
 
