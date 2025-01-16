b57bc3db3664756c3354854ec0895444b522ef86
rhkk7157 <rhkk7157@gmail.com>
2025-01-16 10:45:51 -0800

    add react-icons

--- src/components/about/AboutContent.tsx
+++ src/components/about/AboutContent.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { FaReact } from 'react-icons/fa'; 
+import { SiTypescript } from 'react-icons/si';
+import { RiTailwindCssFill } from "react-icons/ri";
+import { RiNextjsFill } from "react-icons/ri";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
 
 const AboutContent:React.FC = () => {
   return (
@@ -16,15 +22,23 @@ const AboutContent:React.FC = () => {
       <p className='text-sm sm:text-base mt-2 lg:text-lg md:text-lg'>
         To my coworkers, I want to be the person they enjoy working with.
       </p>
-      <p className='text-sm sm:text-base mt-2 lg:text-lg md:text-lg'>
+      {/* <p className='text-sm sm:text-base mt-2 lg:text-lg md:text-lg'>
         While working, I focus on both DX that brings joy to my coworkers and UX that delights users. 
-      </p>
+      </p> */}
       <p className='text-sm sm:text-base mt-2 lg:text-lg md:text-lg'>
         I enjoy meeting people and am interested in working with diverse professionals to create great results. 
       </p>
       <p className='text-sm sm:text-base mt-2 lg:text-lg md:text-lg'>
         I aim for collaboration that satisfies everyone by actively communicating and always looking for areas to improve.
       </p>
+      <div className='flex gap-6 mt-4'>
+        <FaReact className="text-blue-500 text-3xl" title="React" />
+        <SiTypescript className="text-blue-700 text-3xl" title="TypeScript" />
+        <RiTailwindCssFill className="text-blue-700 text-3xl" title="Tailwind" />
+        <RiNextjsFill className="text-blue-700 text-3xl" title="Nextjs" />
+        <FaHtml5 className="text-blue-700 text-3xl" title="html" />
+        <FaCss3Alt className="text-blue-700 text-3xl" title="css" />
+      </div>
     </div>
   )
 }
